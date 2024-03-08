c950210087d83a5408adf0ca05068f710d3037f7
Hashim <hashimkakkaden@gmail.com>
2024-03-08 21:02:06 +0530

    bug fixed in athenticatio

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -6,15 +6,14 @@ const authController = require('../controller/authController');
 const userMessageController = require('../controller/userMessageController')
 const authMiddleware = require('../middlewares/authMiddlewares');
 const alertController = require("../controller/alertsController");
-
-
 userRoutes.post('/sendAlertMessages', alertController.sendAlertMessages);
-
 
 userRoutes.get('/', authMiddleware.isLogout, authController.loadLogIn);
 userRoutes.post('/', authMiddleware.isLogout, authController.handleLogIn)
 userRoutes.get('/signup', authMiddleware.isLogout, authController.loadSignup)
-userRoutes.post('/signup', authMiddleware.isLogout, authController.handleSingUp);
+userRoutes.post('/signup', authMiddleware.isLogout, authController.handleSignUp);
+userRoutes.get('/otp',authMiddleware.isLogin,authController.loadOtpPage)
+userRoutes.patch('/verifyOtp',authController.verifyOtp)
 userRoutes.get('/home',authMiddleware.isLogin,authController.loadHome)
 userRoutes.get('/check-point',userMessageController.reachCheckPoint)
 
