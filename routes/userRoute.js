b6a9a56d3c01bbabeb5d26577757f3da56c945ba
fazil-tv <144906015+fazil-tv@users.noreply.github.com>
2024-03-08 17:17:03 +0530

    ejs files added

---
+++ routes/userRoute.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const userRoute = express();
+
+userRoute.set('view engine','ejs')
+userRoute.set('views', './views/user')
+
+const userRoutes = express();
+
+
+const authController = require('../controller/authController');
+const alertController = require("../controller/alertsController");
+const mapController = require('../controller/mapController');
+
+
+userRoutes.get('/login',authController.loadLogIn);
+userRoutes.post('/login',authController.handleLogIn);
+userRoutes.get('/',authController.home);
+userRoutes.get('/signup',authController.loadSignup);
+userRoutes.post('/signup',authController.handleSingUp);
+userRoute.get('/safetymap', mapController.safetyMap);
+userRoutes.post('/sendAlertMessages', alertController.sendAlertMessages);
+
+module.exports = userRoutes;
