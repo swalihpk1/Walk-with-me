af7164df1962b5c58878455e4dade15d10f3ee53
fazil-tv <144906015+fazil-tv@users.noreply.github.com>
2024-03-09 12:45:35 +0530

    otp ejs integreated

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,11 +1,9 @@
 const express = require("express");
 const userRoute = express();
+const userRoutes = express();
 
 userRoute.set('view engine','ejs')
 userRoute.set('views', './views/user')
-
-const userRoutes = express();
-
 
 const authController = require('../controller/authController');
 const userMessageController = require('../controller/userMessageController')
@@ -13,17 +11,18 @@ const authMiddleware = require('../middlewares/authMiddlewares');
 const alertController = require("../controller/alertsController");
 const mapController = require('../controller/mapController');
 
-
-
 userRoutes.get('/login',authMiddleware.isLogout,authController.loadLogIn);
 userRoutes.post('/login',authMiddleware.isLogout,authController.handleLogIn);
 userRoutes.get('/',authMiddleware.isLogin,authController.home);
 userRoutes.get('/signup',authMiddleware.isLogout,authController.loadSignup);
 userRoutes.post('/signup',authMiddleware.isLogout,authController.handleSignUp);
-userRoute.get('/safetymap',authMiddleware.isLogin, mapController.safetyMap);
-userRoutes.patch('/verifyOtp',authController.verifyOtp)
-userRoutes.post('/sendAlertMessages', alertController.sendAlertMessages);
+userRoutes.get('/safetymap', mapController.safetyMap);
+userRoutes.patch('/verifyOtp',authController.verifyOtp);
 
-userRoutes.get('/check-point',userMessageController.reachCheckPoint)
+userRoutes.get('/otp',authController.loadOtpPage);
+userRoutes.post('/sendAlertMessages', alertController.sendAlertMessages);
+userRoutes.get('/check-point',userMessageController.reachCheckPoint);
+
+
 
 module.exports = userRoutes;
