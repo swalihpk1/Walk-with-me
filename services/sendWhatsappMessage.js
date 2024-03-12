7c0fac9179ad5f06697ef1de63f9f9cb9381ec6c
rithasahmed12 <145911740+rithasahmed12@users.noreply.github.com>
2024-03-12 09:14:44 +0530

    Update sendWhatsappMessage.js

--- services/sendWhatsappMessage.js
+++ services/sendWhatsappMessage.js
@@ -3,18 +3,18 @@ const request = require("request");
 
 const sendWhatsappMessage = (phoneNumber, message) => {
     return new Promise((resolve, reject) => {
-        var options = {
-            'method': 'POST',
-            'url': 'https://whats-api.rcsoft.in/api/create-message',
-            'headers': {
-            },
-            formData: {
-              'appkey': '19639dad-ed9a-443d-99b8-109214637982',
-              'authkey': 't776EID8ysP7Lyj0JuIqWUbrGuoDARPCzsNcQHRxHRqHXehxfd',
-              'to': phoneNumber,
-              'message': message
-            }
-          };
+       var options = {
+          'method': 'POST',
+          'url': 'https://whats-api.rcsoft.in/api/create-message',
+          'headers': {
+          },
+          formData: {
+         'appkey': 'dd0de7c2-575e-458b-b3b6-a7c6985c5dca',
+         'authkey': 'yZWhAmnpu2sJAYw8FYtWB5BBEmKtr5YmGrh12ldXsTEaHEDPCc',
+         'to': 'RECEIVER_NUMBER',
+         'message': 'Example message'
+  }
+};
         console.log(phoneNumber,message)
 
         request(options, function (error, response) {
